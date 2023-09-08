fe8845ab3fd00fdb14c709ddbd0eba0becd228a4
josueRusso <josuerusso1911@gmail.com>
2023-09-08 11:40:50 -0400

    Creando la DAL

---
+++ PrimerParcial/DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerParcial.Model;
+namespace PrimerParcial.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Ingresos> Ingresos { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
